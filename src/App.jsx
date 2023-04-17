f758addc5531ed8dc39887c8f2f0718de293c472
Leo <leonardovrodriguez246@gmail.com>
2023-04-16 23:10:58 -0400

    added useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import ToDoList from './ToDoList';
 import './App.css';
 
  function App() {
-  const todos = ['Learn React', 'Learn MERN stack', 'Take a nap',];
+  // const todos = ['Learn React', 'Learn MERN stack', 'Take a nap',];
+  const todos = useState(['Learn React', 'Learn MERN stack', 'Take a nap'])
+  console.log(todos)
   return (
     <div className="App">
       <h1 id='heading'>React To Do</h1>
-      <ToDoList todos={todos}/>
+      <ToDoList todos={todos[0]}/>
     </div>
     
   );
