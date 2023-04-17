f229d3589f092fd878f95396f218ea636261daf2
Leo <leonardovrodriguez246@gmail.com>
2023-04-17 00:51:59 -0400

    added hide/show button

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import './App.css';
 
  function App() {
   // const todos = ['Learn React', 'Learn MERN stack', 'Take a nap',];
-  const [todos, setTodos] = useState(['Learn React', 'Learn MERN stack', 'Take a nap'])
+  const [todos, setTodos] = useState([
+    'Learn React', 
+    'Learn MERN stack', 
+    'Take a nap'
+  ]);
 
   function handleAddTodo(todo){
     const newArray = [...todos, "hello world"]
@@ -13,11 +17,13 @@ import './App.css';
 
   const [showTodos, setShowTodos] = useState(true)
  
+   
 
   console.log(todos)
   return (
     <div className="App">
       <h1 onClick={handleAddTodo} id='heading'>React To Do</h1>
+      <button onClick={() => setShowTodos(!showTodos)}>{showTodos ? 'HIDE' : 'SHOW'}</button>
       {showTodos && <ToDoList todos={todos}/>}
     </div>
     
